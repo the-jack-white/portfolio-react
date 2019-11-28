e9d5cd937934083cff438b0e53f909c332ac2d0f
the-jack-white <white123jack@gmail.com>
2019-11-29 00:31:20 +0200

    Remove Particles from small screens

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import particlesParams from './Particles/particlesParams';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -8,24 +8,41 @@ import './App.css';
 import Welcome from './Welcome/Welcome';
 import Navigation from './Navigation/Navigation';
 
-const App = () => {
-    return(
-        <BrowserRouter>
-            <div className="main-container">
-                <div className="particle-container">
-                    <Particles 
-                        params={ particlesParams }
-                    />
+class App extends Component {
+
+    state = {
+        matches: window.matchMedia("(min-width: 768px)").matches
+    }
+
+    componentDidMount() {
+        const handler = e => this.setState({matches: e.matches});
+        window.matchMedia("(min-width: 768px)").addListener(handler);
+    }
+
+    render() {
+        return(
+            <BrowserRouter>
+                <div className="main-container">
+                    {this.state.matches && (
+                        <div className="particle-container">
+                            <Particles 
+                                params={ particlesParams }
+                            />
+                        </div>
+                    )}
+
+                    {!this.state.matches && (<p></p>)}
+                    
+                    <Navigation />
+                    <div className="content-container">
+                        <Switch>
+                            <Route exact path='/' component={Welcome} />
+                        </Switch>
+                    </div>
                 </div>
-                <Navigation />
-                <div className="content-container">
-                    <Switch>
-                        <Route exact path='/' component={Welcome} />
-                    </Switch>
-                </div>
-            </div>
-        </BrowserRouter>
-    )
+            </BrowserRouter>
+        )
+    }
 }
 
 export default App;
