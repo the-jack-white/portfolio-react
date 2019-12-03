990f1c64f675c98f9a7ed7f94c53ab6c883a85b2
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-12-03 17:25:11 +0200

    Skills Styling

--- frontend/src/Components/Context/index.js
+++ frontend/src/Components/Context/index.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import Skills from './skills.json';
 
 const PortfolioContext = React.createContext();
 
 export class Provider extends Component {
 
-    state = {
-        skills: Skills.data
-    }
-
     render() {
         return(
             <PortfolioContext.Provider value={{
-                data: {
-                    skills: this.state.skills
-                }
+                
             }}>
                 {this.props.children}
             </PortfolioContext.Provider>
--- frontend/src/Components/Skills/Skills.js
+++ frontend/src/Components/Skills/Skills.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { Consumer } from '../Context';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import './Skills.css';
 
 const Skills = () => {
     return (
-        <Consumer>
-            { context => {
-                console.log(context)
-                return (
-                    <h3>Hello World</h3>
-                )
-            }}
-        </Consumer>
+        <Container >
+            <Row>
+                <Col>
+                    <ul>
+                        <li>ReactJS</li>
+                        <li>ES6 JavaScript</li>
+                        <li>DOM manipulation</li>
+                        <li>jQuery</li>
+                        <li>AJAX</li>
+                        <li>JSON</li>
+                        <li>NodeJS</li>
+                        <li>npm</li>
+                        <li>Express</li>
+                        <li>MongoDB</li>
+                        <li>Unified Modeling Language (UML)</li>
+                    </ul>
+                </Col>
+                <Col>
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>Bootstrap4</li>
+                        <li>WordPress</li>
+                        <li>Shopify</li>
+                        <li>SEO</li>
+                        <li>Object Orientated Programming</li>
+                        <li>HTTP Basics</li>
+                        <li>Ubuntu Linux</li>
+                        <li>Python</li>
+                        <li>AWS Products (S3, Rekognition, Lambda)</li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
