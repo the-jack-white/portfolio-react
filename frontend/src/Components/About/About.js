9447590838dca5380e66654c27187c8cb753b0d6
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-12-04 11:33:50 +0200

    Update About Component
    Remove the LindedInBadge Component state from the About Component

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -6,16 +6,12 @@ import LinkedinBadge from './LinkedinBadge';
 class About extends Component {
 
     state = {
-        matches: window.matchMedia("(min-width: 768px)").matches,
-        mountBadge: false
+        matches: window.matchMedia("(min-width: 768px)").matches
     }
 
     componentDidMount() {
         const handler = e => this.setState({matches: e.matches});
         window.matchMedia("(min-width: 768px)").addListener(handler);
-        this.setState({
-            mountBadge: true
-        })
     }
 
     render() {
@@ -29,9 +25,7 @@ class About extends Component {
                                 Eye-poppingly prolific, full stack developer with a passion for metrics and information. I have an arsenal of tools and languages in my disposal to find solutions to all kinds of different problems. I worked on an average of 15 different projects for Biz2Click. The largest projects I've worked on is for HiDesign Group.
                             </p>
                         </div>
-                        {this.state.mountBadge && (
-                            <LinkedinBadge />
-                        )}
+                        <LinkedinBadge />
                     </div>  
                 )}
                 {!this.state.matches && (
