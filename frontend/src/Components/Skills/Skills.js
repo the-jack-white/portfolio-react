02e4bfcbc14fd0119a14c0cfd08b5782511128ea
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-12-04 11:49:19 +0200

    Adjustments for Mobile View in the Skills Component

--- frontend/src/Components/Skills/Skills.js
+++ frontend/src/Components/Skills/Skills.js
@@ -5,7 +5,7 @@ import './Skills.css';
 
 const Skills = () => {
     return (
-        <Container >
+        <Container className="skills-container">
             <Row>
                 <Col>
                     <ul>
@@ -43,33 +43,3 @@ const Skills = () => {
 }
 
 export default Skills;
-
-{/* <div className="skills-container">
-            <ul>
-                <li>ReactJS</li>
-                <li>ES6 JavaScript</li>
-                <li>DOM manipulation</li>
-                <li>jQuery</li>
-                <li>AJAX</li>
-                <li>JSON</li>
-                <li>NodeJS</li>
-                <li>npm</li>
-                <li>Express</li>
-                <li>MongoDB</li>
-                <li>Unified Modeling Language (UML)</li>
-            </ul>
-
-            <ul>
-                <li>HTML5</li>
-                <li>CSS3</li>
-                <li>Bootstrap4</li>
-                <li>WordPress</li>
-                <li>Shopify</li>
-                <li>SEO</li>
-                <li>Object Orientated Programming</li>
-                <li>HTTP Basics</li>
-                <li>Ubuntu Linux</li>
-                <li>Python</li>
-                <li>AWS Products (S3, Rekognition, Lambda)</li>
-            </ul>
-        </div> */}
