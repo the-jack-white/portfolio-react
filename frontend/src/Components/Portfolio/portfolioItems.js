d4ed32d5e6b8b19fed1f9d71fd982082c54614fb
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2020-03-12 09:28:01 +0200

    2020 Updates

--- frontend/src/Components/Portfolio/portfolioItems.js
+++ frontend/src/Components/Portfolio/portfolioItems.js
@@ -12,6 +12,85 @@ const biz2click = [
         url: 'https://www.thealarmguy.co.za/'
     },
     {
+        name: "HiDesign MEA",
+        id: 3,
+        image: '/myportfolio/img/mea.png',
+        url: 'https://hidesign-mea.com/'
+    },
+    {
+        name: "HiDesign Asia",
+        id: 4,
+        image: '/myportfolio/img/asia.png',
+        url: 'https://hidesign-asia.com/'
+    },
+    {
+        name: "HiDesign Europe",
+        id: 5,
+        image: '/myportfolio/img/europe.png',
+        url: 'https://www.hidesigneurope.com/'
+    },
+    {
+        name: "HiDesign Americas",
+        id: 6,
+        image: '/myportfolio/img/americas.png',
+        url: 'https://www.hidesign-americas.com/'
+    },
+    {
+        name: "Cape Construction Expo",
+        id: 8,
+        image: '/myportfolio/img/capeConstruction.png',
+        url: 'https://www.capeconstructionexpo.com/'
+    },
+    {
+        name: "KZN Construction",
+        id: 9,
+        image: '/myportfolio/img/kznConstruction.png',
+        url: 'https://www.kznconstructionexpo.com/'
+    },
+    {
+        name: "Kristen Lalieu",
+        id: 10,
+        image: '/myportfolio/img/kristen.png',
+        url: 'https://kristenlalieu.com/'
+    },
+    {
+        name: "MSI",
+        id: 11,
+        image: '/myportfolio/img/msi.png',
+        url: 'https://safconsult.africaforce.com/'
+    },
+    {
+        name: "Briisk",
+        id: 12,
+        image: '/myportfolio/img/briisk.png',
+        url: 'http://www.briisk.io/'
+    },
+    // {
+    //     name: "Bentley Associates",
+    //     id: 13,
+    //     image: '/myportfolio/img/bentleyAs.png',
+    //     url: 'https://bentleyassociates.co.uk/'
+    // },
+]
+
+export default biz2click;
+
+/*
+
+const biz2click = [
+    {
+        name: "Business Connexion",
+        id: 1,
+        image: '/myportfolio/img/bcx.png',
+        url: 'https://www.bcx.co.za/'
+    },
+    {
+        name: "The Alarm Guy",
+        id: 2,
+        image: '/myportfolio/img/alarm.png',
+        url: 'https://www.thealarmguy.co.za/'
+    },
+    {
         name: "Kristen Lalieu",
         id: 3,
         image: '/myportfolio/img/kristen.png',
@@ -49,4 +128,4 @@ const biz2click = [
     },
 ]
 
-export default biz2click;
+*/
